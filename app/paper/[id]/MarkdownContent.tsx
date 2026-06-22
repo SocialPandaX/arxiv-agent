566b4dc45b321eeaa402047dbb5fcc24fd37b202
SocialPandaX <mpakawm@163.com>
2026-06-23 00:22:07 +0800

    Add GFM support for markdown tables rendering

--- app/paper/[id]/MarkdownContent.tsx
+++ app/paper/[id]/MarkdownContent.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
 import rehypeKatex from 'rehype-katex'
 import 'katex/dist/katex.min.css'
@@ -9,7 +10,7 @@ export default function MarkdownContent({ content }: { content: string }) {
   return (
     <div className="prose prose-slate max-w-none">
       <ReactMarkdown
-        remarkPlugins={[remarkMath]}
+        remarkPlugins={[remarkGfm, remarkMath]}
         rehypePlugins={[rehypeKatex]}
       >
         {content}
